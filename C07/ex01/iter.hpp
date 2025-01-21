bd7ce2377ff792fd2a4ed7782c8432d766611944
Pierotti Falli <pfalli@c2r8s1.42wolfsburg.de>
2025-01-21 15:47:38 +0100

    c07 added tests from eval

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -5,14 +5,26 @@
 #include <string>
 
 template <typename anytype>
-void iter(anytype *array, int length, void (*function)(anytype &)) { // why *ptr
+void iter(anytype *array, int length, void (*function)(anytype &)) {
 	for (int i = 0; i < length; i++) {
 		function(array[i]);
 	}
 }
 
 template <typename anytype>
-void function(anytype &array) { // why &
+void function(anytype &array) {
+	std::cout << array << std::endl;
+}
+
+template <typename anytype>
+void iter(anytype *array, int length, void (*function)(const anytype &)) {
+	for (int i = 0; i < length; i++) {
+		function(array[i]);
+	}
+}
+
+template <typename anytype>
+void function(const anytype &array) {
 	std::cout << array << std::endl;
 }
 
